7eb79825528ab2552b12eaa7a7ea607b9a82add3
uzi <76636838+uziverse@users.noreply.github.com>
2020-12-26 23:56:36 -0300

    Update port2.py

--- port2.py
+++ port2.py
@@ -15,7 +15,7 @@ def scan():
    print("")
    print("\033[1;95mfazendo a varredura, aguarde...")
    sleep(sec)
-   for porta in range(79,65535):
+   for porta in range(21,65535):
       s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       if s.connect_ex((ip,porta)) == 0:
          print(porta, "\033[1;32mABERTA!")
